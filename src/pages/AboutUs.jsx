3374357139a775b8370d70617161220e4ea9528d
Amruta <amrutabmaskar@gmail.com>
2025-02-14 13:20:32 +0530

    create navbar properly

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
+import Navbar from '../components/Navbar/Navbar'
 
 function AboutUs() {
   return (
     <div>
        <Navbar />
         <h1>About Us</h1>
-    </div>
+     </div>
+    
   )
 }
 
