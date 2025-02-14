71df4db7b3d708361d9020892b6dae846d052d2f
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2025-02-14 20:11:07 +0530

    implement role based filtering

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import petimg from '../../src/assets/petimg.jpg';
+import petimg2 from '../../src/assets/petimg2.jpg';
+
 import { Teams } from './TeamsData';
 
 
@@ -7,7 +9,8 @@ import { Teams } from './TeamsData';
 const AboutUs = ({organizationName})=>{
 const [searchText, setsearchText] = useState("");
 const [filterdUsers, setfilterdUsers] = useState(Teams);
-
+const [filterRole, setfilterRole] = useState("");
+ 
 useEffect(()=>{
   if(!searchText){
     setfilterdUsers(Teams);
@@ -34,25 +37,76 @@ useEffect(()=>{
 
 },[searchText])
 
+useEffect(()=>{
+  if(!filterRole){
+    setfilterdUsers(Teams);
+    return;
+    }
+    const tempfilterdUsers = Teams.filter((team)=>{
+      if(filterRole && team.role=== filterRole){
+        return true;
+        }
+        else{
+          return false;
+          }
+    });
+    setfilterdUsers(tempfilterdUsers);
+},[filterRole])
+
 
   return (
-    <div className=' bg-gray-100 full-h-screen '>
-    <img src={petimg} alt='pet image' className="w-[1600px] h-[500px] " />
+    <div className=' bg-white full-h-screen '>
+    <img src={petimg} alt='pet image' className="w-[1600px] h-[400px] " />
     <div className='bg-white mt-3'>
-    <h1 className='text-center text-4xl text-purple-600 bg-white'><b>About Us</b></h1>
+    
     <p className='text-center mt-5 text-2xl bg-white'>
-        Welcome to <span className='font-semobolad text-purple-500'>{organizationName}</span>, a loving home for pets in need. 
-          Our mission is to connect rescue animals with caring individuals and families, ensuring every pet 
-          finds a forever home filled with love and happiness.
+        Welcome to <span className='font-semobolad text-purple-600'>{organizationName}</span>, a loving home for pets in need. 
+          
       </p>
     </div> 
-    <h2 className='text-center text-3xl m-5 text-purple-400'>LEADERSHIP TEAM</h2>
+    
+   
+    <div className='bg-gray-100 mt-5 flex justify-around'>
+    
+    <ui className=' text-2xl mt-20 text-purple-400 text-justify'>
+      <h2 className='txet-2xl m-2 ml-15 text-black'>Our Mission</h2>
+      <li>To prevent cruelty to animals</li>
+      <li>To protect them from injury.</li>
+      <li>To promote animal care and welfare.</li>
+      <li>To foster the human-animal relationship.</li>
+      <li>To inculcate humanistic values among humans.</li>
+    </ui>
 
+<img src={petimg2} alt='pet image2' className='w-[500px] m-4'/>
+</div>
+    <h2 className='text-center text-3xl m-5 text-purple-500'><b>Our TEAM</b></h2>
+    
     <input type='text' placeholder='Search' className='w-2/3 p-2 mb-5 bg-white block mx-auto rounded-lg text-2xl border border-purple-100' value={searchText}
     onChange={(e)=>setsearchText(e.target.value)} />
+       <div className='ml-150 mb-5 '>
+      <span className='text-2xl'>Filter By Position:</span>
+      <select className='text-lg my-2  rounded-lg px-5 text-purple-500' value={filterRole}
+      onChange={(e)=>setfilterRole(e.target.value)}>
+
+
+      
+      
+        <option value='all'>all</option>
+        {
+          Teams.map((team)=>{
+            return <option value={team.role} key={team.role}>{team.role}</option>
+          })
+        }
+       
+        
+      </select>
+      </div>
+    
+
+
     
     
-    <div className="flex flex-wrap justify-around">
+    <div className="flex flex-wrap justify-around bg-gray-100">
       
       {filterdUsers.map((userData, index)=>{
       const {name,role,avatar} = userData;
