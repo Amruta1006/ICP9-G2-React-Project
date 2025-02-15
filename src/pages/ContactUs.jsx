248d7fd9dd92e7ed65150260097f848916bde037
juned012 <junedansari9172@gmail.com>
2025-02-15 14:31:52 +0530

    remove Navbar component from Contact Us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,10 +1,6 @@
-import React from "react";
-import Navbar from "../components/Navbar/Navbar";
-
 function ContactUs() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-10">
-      <Navbar />
       <div className="container mx-auto p-6 max-w-5xl">
         <h1 className="text-4xl font-bold text-center text-gray-900 mb-8 underline decoration-wavy decoration-pink-400">
           Contact Us
