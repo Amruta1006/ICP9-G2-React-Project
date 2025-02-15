80ec7436a80662b700d87ede3ed66682afc8d8c8
juned012 <junedansari9172@gmail.com>
2025-02-15 12:43:49 +0530

    enhance Contact Us

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 function ContactUs() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50 py-10">
       <Navbar />
-        <h1>Contact Us</h1> 
+      <div className="container mx-auto p-6 max-w-5xl">
+        <h1 className="text-4xl font-bold text-center text-gray-900 mb-8 underline decoration-wavy decoration-pink-400">
+          Contact Us
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
