d714ec3b225e9dffd99636e77195b67eee86430d
DESKTOP-8L5OUK3\LENOVO <tousifjamadar52a@gmail.com>
2025-02-15 19:14:56 +0530

    Added heading on success stories page

--- src/pages/SuccessStories.jsx
+++ src/pages/SuccessStories.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
-
 function SuccessStories() {
   return (
-    <div>
-        <h1>Success Stories</h1>
+    <div className="min-h-screen p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">🐾 Adoption Success Stories</h1>
     </div>
-  )
+  );
 }
 
-export default SuccessStories
+export default SuccessStories;
