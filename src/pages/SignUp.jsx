1b5e548642a31e13b22c7b0aef585735a4abe96e
GitanjaliSase <sasegitanjali20@gmail.com>
2025-02-16 00:00:26 +0530

    add heading

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 
-  
+const SignUp = () => {
+ 
+
+  return (
+    <div className="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg mt-10">
+      <h2 className="text-xl font-bold mb-4">Sign Up</h2>
+    
+    </div>
+  );
+};
 
 export default SignUp;
