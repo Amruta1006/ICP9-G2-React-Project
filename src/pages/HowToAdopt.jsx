7e49e4d8de3b75a1c0260fc50a5a1af6f68f36eb
Bhakti Dethe <bhaktidethe956@gmial.com>
2025-02-16 10:12:24 +0530

    H1 rag added

--- src/pages/HowToAdopt.jsx
+++ src/pages/HowToAdopt.jsx
@@ -3,5 +3,8 @@ import { FaPaw, FaSearch, FaClipboardCheck, FaHome } from "react-icons/fa";
 import { toast, Toaster } from "react-hot-toast";
 
 function HowToAdopt() {
-    
+    <div className="min-h-screen flex flex-col">
+    {/* Main Content */}
+    <div className="bg-gradient-to-b from-pink-100 to-white py-16 flex-grow">
+      <h1 className="text-5xl font-bold text-center text-black mb-6">How to Adopt a Pet</h1>
 }
