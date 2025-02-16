a9a49d427c74772644a379ca3d95704c6dc44ea8
Bhakti Dethe <bhaktidethe956@gmial.com>
2025-02-16 10:43:38 +0530

    done

--- src/pages/HowToAdopt.jsx
+++ src/pages/HowToAdopt.jsx
@@ -3,8 +3,9 @@ import { FaPaw, FaSearch, FaClipboardCheck, FaHome } from "react-icons/fa";
 import { toast, Toaster } from "react-hot-toast";
 
 function HowToAdopt() {
+  return(
     <div className="min-h-screen flex flex-col">
-    {/* Main Content */}
+    
     <div className="bg-gradient-to-b from-pink-100 to-white py-16 flex-grow">
       <h1 className="text-5xl font-bold text-center text-black mb-6">How to Adopt a Pet</h1>
 
@@ -46,7 +47,7 @@ function HowToAdopt() {
         </div>
       </div>
 
-      {/* Notification */}
+    
       <Toaster />
     </div>
   );
