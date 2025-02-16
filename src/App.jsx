d947fd039029aa5bba42e740a960954e4bb96d84
Amruta <amrutabmaskar@gmail.com>
2025-02-16 12:00:55 +0530

    remove conflicts from app and main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AdoptPet from "./pages/AdoptPet";
+import ContactUs from "./pages/ContactUs";
+import HowToAdopt from "./pages/HowToAdopt";
+import PetDetails from "./pages/PetDetails";
+import SuccessStories from "./pages/SuccessStories";
+import UserDashboard from "./pages/UserDashboard";
+import AboutUs from "./pages/AboutUs"; 
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/adoptpet" element={<AdoptPet />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/howtoadopt" element={<HowToAdopt />} />
+          <Route path="/petdetails" element={<PetDetails />} />
+          <Route path="/successstories" element={<SuccessStories />} />
+          <Route path="/userdashboard" element={<UserDashboard />} />
+        </Routes>
+      </div>
+      <Footer /> 
+    </div>
+  )
+}
+
+export default AdoptPet;
 
 
