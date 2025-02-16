b708ba48e24e202179540b781005c97bb08c98e1
Bhakti Dethe <bhaktidethe956@gmial.com>
2025-02-16 10:15:54 +0530

    icons added

--- src/pages/HowToAdopt.jsx
+++ src/pages/HowToAdopt.jsx
@@ -7,4 +7,10 @@ function HowToAdopt() {
     {/* Main Content */}
     <div className="bg-gradient-to-b from-pink-100 to-white py-16 flex-grow">
       <h1 className="text-5xl font-bold text-center text-black mb-6">How to Adopt a Pet</h1>
+
+      <p className="text-center text-black text-lg max-w-3xl mx-auto mb-12">
+          Adopting a pet is a life-changing experience. Follow these simple
+           steps to bring home a new furry friend! 🐶🐱🐰
+        </p>
+
 }
